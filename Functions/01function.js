7279abf55b759d51451db291097e8dbe504a4d8a
Lakshay Chandna <lakshay13chandna@gmail.com>
2025-05-11 22:45:57 +0530

    Add inbuilt functions example and clarify return statement usage

--- Functions/01function.js
+++ Functions/01function.js
@@ -11,3 +11,7 @@ for(leti=1; i<=20; i++){
     console.log(i + " is " + isEvenOrOdd(i));
 }
 // console.log(isEvenOrOdd(10)); // even
+
+// Q) why do we doing return
+
+// console.log has a functionaly of displayingg the output on the console but return is used to return the value from the function.
